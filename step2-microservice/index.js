29656f091f1ce7df4fcfd65601fae3695705e419
Mishael Ratemo <ratemomishael@gmail.com>
2023-04-05 15:13:06 +0300

    configuration service to run from env variables

--- step2-microservice/index.js
+++ step2-microservice/index.js
@@ -1,27 +1,36 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
-const port = 3000;
+// Throws an error if the PORT environment variable is missing.
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
 
-app.get('/video',(req, res) => {
-    const path="~/Microservices/SampleVideo_1280x720_5mb.mp4";
-    fs.stat(path, (err, stats) =>{
-        if (err){
-            console.error('An Error has ocurred');
+// Extracts the PORT environment variable.
+const PORT = process.env.PORT;
+
+// Registers a HTTP GET route for video streaming.
+app.get("/video", (req, res) => {
+
+    const path = "../video/vid.mp4";
+    fs.stat(path, (err, stats) => {
+        if (err) {
+            console.error("An error occurred ");
             res.sendStatus(500);
             return;
         }
-        res.writeHead(200,{
-            'Content-Length':stats.size,
-            'Content-Type': 'video/mp4',
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
         });
         fs.createReadStream(path).pipe(res);
     });
 });
 
-app.listen(port, () => {
-    console.log(`App is listening on port ${port}!!`);
-})
+// Starts the HTTP server.
+app.listen(PORT, () => {
+    console.log(`Microservice listening on port ${PORT}, point your browser at http://localhost:${PORT}/video`);
+});
